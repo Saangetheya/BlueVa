a834a700de11a0d200bb1695d8f1bae83869dbd2
sandeepCS06 <100673838+sandeepCS06@users.noreply.github.com>
2023-04-15 21:13:00 +0530

    arrow

--- R1/BlueVa/src/main/java/com/example/blueva/Curve.java
+++ R1/BlueVa/src/main/java/com/example/blueva/Curve.java
@@ -1,68 +1,97 @@
 package com.example.hellofx;
 
 import javafx.application.Application;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 public class Curve extends Application {
-    private double startX, startY;
-    private Line line;
+    private double startX, startY, endX, endY;
+    private Path line;
+    private Label label1, label2;
+    private boolean lineVisible = false;
 
-    public void start(Stage stage) {
-        line = new Line();
-        line.setStartX(100);
-        line.setStartY(100);
-        line.setEndX(100);
-        line.setEndY(100);
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        label1 = new Label("Label 1");
+        label2 = new Label("Label 2");
+        label1.setTranslateX(50);
+        label1.setTranslateY(50);
+        label2.setTranslateX(300);
+        label2.setTranslateY(200);
+        pane.getChildren().addAll(label1, label2);
 
-        Circle startNode = createNode(line.getStartX(), line.getStartY());
-        Circle endNode = createNode(line.getEndX(), line.getEndY());
+        label1.setOnMousePressed(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                startX = label1.getTranslateX() + label1.getWidth() / 2;
+                startY = label1.getTranslateY() + label1.getHeight() / 2;
+                endX = startX;
+                endY = startY;
+                line = new Path();
+                line.setStrokeWidth(2);
+                line.setStroke(Color.BLACK);
+                pane.getChildren().add(line);
+                lineVisible = true;
+            }
+        });
 
-        Pane pane = new Pane(line, startNode, endNode);
+        label1.setOnMouseDragged(event -> {
+            if (event.getButton() == MouseButton.PRIMARY && lineVisible) {
+                endX = event.getX();
+                endY = event.getY();
+                updateArrowLine();
+            }
+        });
+
+        label1.setOnMouseReleased(event -> {
+            if (event.getButton() == MouseButton.PRIMARY && lineVisible) {
+                boolean isLineTouchingLabel2 = line.intersects(label2.getBoundsInParent());
+                if (isLineTouchingLabel2) {
+                    // Line touches Label 2, update the arrow line
+                    endX = label2.getTranslateX() + label2.getWidth() / 2;
+                    endY = label2.getTranslateY() + label2.getHeight() / 2;
+                    updateArrowLine();
+                } else {
+                    // Line released outside of Label 2, remove line
+                    pane.getChildren().remove(line);
+                    lineVisible = false;
+                }
+                line = null; // Reset the line reference
+            }
+        });
+
+
         Scene scene = new Scene(pane, 400, 400);
-
-        // event handlers for start node
-        startNode.setOnMousePressed(event -> {
-            startX = event.getX();
-            startY = event.getY();
-        });
-        startNode.setOnMouseDragged(event -> {
-            double offsetX = event.getX() - startX;
-            double offsetY = event.getY() - startY;
-            line.setStartX(line.getStartX() + offsetX);
-            line.setStartY(line.getStartY() + offsetY);
-            startX = event.getX();
-            startY = event.getY();
-        });
-
-        // event handlers for end node
-        endNode.setOnMousePressed(event -> {
-            startX = event.getX();
-            startY = event.getY();
-        });
-        endNode.setOnMouseDragged(event -> {
-            double offsetX = event.getX() - startX;
-            double offsetY = event.getY() - startY;
-            line.setEndX(line.getEndX() + offsetX);
-            line.setEndY(line.getEndY() + offsetY);
-            startX = event.getX();
-            startY = event.getY();
-        });
-
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Arrow Line");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    private Circle createNode(double x, double y) {
-        Circle node = new Circle(x, y, 5, Color.RED);
-        node.setCursor(Cursor.HAND);
-        return node;
+    private void updateArrowLine() {
+        double arrowSize = 10; // Size of the arrowhead
+        double angle = Math.atan2(endY - startY, endX - startX); // Angle of the line
+        double sin = Math.sin(angle);
+        double cos = Math.cos(angle);
+
+        // Calculate the coordinates of the arrowhead points
+        double x1 = endX - arrowSize * cos - arrowSize * sin;
+        double y1 = endY - arrowSize * sin + arrowSize * cos;
+        double x2 = endX - arrowSize * cos + arrowSize * sin;
+        double y2 = endY - arrowSize * sin - arrowSize * cos;
+
+        // Update the arrow line
+        line.getElements().clear();
+        line.getElements().add(new MoveTo(startX, startY));
+        line.getElements().add(new LineTo(endX, endY));
+        line.getElements().add(new MoveTo(endX, endY));
+        line.getElements().add(new LineTo(x1, y1));
+        line.getElements().add(new MoveTo(endX, endY));
+        line.getElements().add(new LineTo(x2, y2));
     }
 
     public static void main(String[] args) {
