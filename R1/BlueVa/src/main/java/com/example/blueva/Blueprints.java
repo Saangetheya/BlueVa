fd46ca43250016baadd8730c72ebdd95311682b2
Abhijith Shaji <88903559+Saangetheya@users.noreply.github.com>
2023-05-05 11:36:06 +0530

    Delete R1/BlueVa/src/main directory

--- R1/BlueVa/src/main/java/com/example/blueva/Blueprints.java
+++
@@ -1,4 +0,0 @@
-package com.example.blueva;
-
-public class Blueprints {
-}
--- R1/BlueVa/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.blueva {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.blueva to javafx.fxml;
-    exports com.example.blueva;
-}
